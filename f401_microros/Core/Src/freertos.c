4d613c7b7cb2312d925482e0fae0b0b3cfed6f10
aleg3214 <alessandrog.3214@gmail.com>
2024-10-08 18:29:03 +0200

    first commit

---
+++ f401_microros/Core/Src/freertos.c
@@ -0,0 +1,246 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usart.h"
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <uxr/client/transport.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include <std_msgs/msg/int32.h>
+#include <geometry_msgs/msg/twist.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+uint32_t defaultTaskBuffer[ 3000 ];
+osStaticThreadDef_t defaultTaskControlBlock;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .cb_mem = &defaultTaskControlBlock,
+  .cb_size = sizeof(defaultTaskControlBlock),
+  .stack_mem = &defaultTaskBuffer[0],
+  .stack_size = sizeof(defaultTaskBuffer),
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
+
+void (* rclc_subscription_callback)(const void *);
+
+void subscription_callback(const void * msgin){
+	    // Cast received message to used type
+	    const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+	    //HAL_GPIO_TogglePin(LD1_GPIO_Port , LD1_Pin);
+
+	    // Process message
+	    //printf("Received: %d\n", msg->linear.x);
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time){
+	RCLC_UNUSED(last_call_time);
+}
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+	// micro-ROS configuration
+
+		  rmw_uros_set_custom_transport(
+		    true,
+		    (void *) &huart2,
+		    cubemx_transport_open,
+		    cubemx_transport_close,
+		    cubemx_transport_write,
+		    cubemx_transport_read);
+
+		  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+		  freeRTOS_allocator.allocate = microros_allocate;
+		  freeRTOS_allocator.deallocate = microros_deallocate;
+		  freeRTOS_allocator.reallocate = microros_reallocate;
+		  freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
+
+		  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+		      printf("Error on default allocators (line %d)\n", __LINE__);
+		  }
+
+		  // micro-ROS app
+
+		  rcl_subscription_t subscriber;
+		  const char * topic_name = "cmd_vel";
+		  geometry_msgs__msg__Twist twist_msg;
+
+		  rcl_publisher_t publisher;
+		  std_msgs__msg__Int32 msg;
+
+
+		  rclc_support_t support;
+		  rcl_allocator_t allocator;
+		  rcl_node_t node;
+
+		  allocator = rcl_get_default_allocator();
+
+		  //create init_options
+		  rclc_support_init(&support, 0, NULL, &allocator);
+
+		  // create node
+		  rclc_node_init_default(&node, "cubemx_node", "", &support);
+
+		  // create publisher
+		  rclc_publisher_init_default(
+		      &publisher,
+		      &node,
+		      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		      "cubemx_publisher");
+
+		  // create subscriber for Twist messages
+		  rclc_subscription_init_default(
+		    &subscriber, &node,
+			ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), topic_name);
+
+		  //Add subscription to the executor
+		  rcl_timer_t timer = rcl_get_zero_initialized_timer();
+		  	const unsigned int timer_timeout = 200;
+		  	rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback);
+
+		  	// create executor
+		  	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+		  	rclc_executor_init(&executor, &support.context, 2, &allocator);
+
+		  	unsigned int rcl_wait_timeout = 1000;   // in ms
+		  	rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout));
+		  	rclc_executor_add_timer(&executor, &timer);
+		  	rclc_executor_add_subscription(&executor, &subscriber, &twist_msg, subscription_callback, ON_NEW_DATA);
+
+		  	//rclc_executor_spin(&executor); //Blocking subscriber
+
+		  msg.data = 0;
+		  for(;;)
+		  {
+			  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)); //Non blocking spin for subscriber
+			  rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+			  	  if (ret != RCL_RET_OK){
+			  		  printf("Error publishing (line %d)\n", __LINE__);
+			  	  }
+
+
+			  msg.data++;
+			  osDelay(10);
+		  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
